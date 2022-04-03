2b9922b7101ff3b1622adf1f0eff401f08ac1ebf
Bruno Fauth <149593@upf.br>
2022-04-03 19:45:55 -0300

    updated fish completions file

--- shared_files/fish_completion.fish
+++ shared_files/fish_completion.fish
@@ -1,22 +1,33 @@
 complete --command=yt_upload --exclusive --short-option=t, --long-option=title --description="defaults to the video's basename"
-complete --command=yt_upload --exclusive --short-option=g, --long-option=genre --description="tag the video as 'GENRE'; see '-G'"
 complete --command=yt_upload --exclusive --short-option=d, --long-option=description --description="set the video's description"
 complete --command=yt_upload --exclusive --short-option=p, --long-option=playlist --description="add video to PLAYLIST, creating it if needed"
 complete --command=yt_upload --exclusive --short-option=T, --long-option=tag --description="tag the vide as 'TAG'; may be used many times"
-complete --command=yt_upload --exclusive --short-option=D, --long-option=date-to-publish --description="formatted like '%Y-%m-%d %H:%M:%S'"
+complete --command=yt_upload --exclusive --short-option=D, --long-option=date-to-publish --description="publish on '%Y-%m-%d %H:%M:%S'"
 complete --command=yt_upload --exclusive --short-option=r, --long-option=recording-date --description="formatted like '%Y-%m-%d %H:%M:%S'"
-complete --command=yt_upload --exclusive --short-option=l, --long-option=language --description="ISO 639-1: <en|fr|de|pt|...>"
+complete --command=yt_upload --exclusive --short-option=l, --long-option=language --description="UI language as <en|fr|de|pt|...>"
 complete --command=yt_upload --exclusive --short-option=a, --long-option=audio-language --description="ISO 639-1: <en|fr|de|pt|...>"
 complete --command=yt_upload --exclusive --short-option=e, --long-option=embeddable --description="make video is embeddable"
-complete --command=yt_upload --exclusive --short-option=S, --long-option=client-secrets-pass --description="client secrets entry in 'pass'"
-complete --command=yt_upload --exclusive --short-option=C, --long-option=credentials-pass --description="credentials entry in 'pass'"
 
-complete --command=yt_upload --short-option=G, --long-option=list-genres --description="list available genres and exit" \
-    --arguments=(yt_upload -G)
+set -l available_genres (yt_upload -G 2>/dev/null)
+complete --command=yt_upload --exclusive --short-option=g, --long-option=genre --description="tag the video as 'GENRE'; see '-G'" \
+    --arguments="$available_genres"
 complete --command=yt_upload --exclusive --short-option=L, --long-option=license --description="[youtube|creativeCommon]" \
     --arguments="youtube creativeCommon"
 complete --command=yt_upload --exclusive --short-option=V, --long-option=visibility --description="[public|unlisted|private]" \
     --arguments="public unlisted private"
+
+if [ -n $PASSWORD_STORE_DIR ]
+    set -l pass_data (find $PASSWORD_STORE_DIR -type f -name "*.gpg" -printf '%P\n' | sed 's|.gpg$||g')
+    complete --command=yt_upload --exclusive --short-option=S, --long-option=client-secrets-pass \
+        --description="client secrets entry in 'pass'" --arguments="$pass_data"
+    complete --command=yt_upload --exclusive --short-option=C, --long-option=credentials-pass \
+        --description="credentials entry in 'pass'" --arguments="$pass_data"
+else
+    complete --command=yt_upload --exclusive --short-option=S, --long-option=client-secrets-pass \
+        --description="client secrets entry in 'pass'"
+    complete --command=yt_upload --exclusive --short-option=C, --long-option=credentials-pass \
+        --description="credentials entry in 'pass'"
+end
 
 complete --command=yt_upload --require-parameter --short-option=s, --long-option=client-secrets-file --description="Secrets JSON file"
 complete --command=yt_upload --require-parameter --short-option=c, --long-option=credentials-file --description="Credentials JSON file"
@@ -27,6 +38,7 @@ complete --command=yt_upload --long-option=location-latitude  --exclusive --desc
 complete --command=yt_upload --long-option=location-longitude --exclusive --description="longitude of video recording, as float"
 complete --command=yt_upload --long-option=location-altitude  --exclusive --description="altitude of video recording, as float"
 
+complete --command=yt_upload --short-option=G, --long-option=list-genres --description="list available genres and exit"
 complete --command=yt_upload --long-option=version --description="show the version and exit"
 complete --command=yt_upload --long-option=help --description="Show a help message and exit"
 
